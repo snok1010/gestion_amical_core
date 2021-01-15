7efb05baa4ad3f29f75954e28b9219d6962d4a38
v.bloch <valentin.bloch@etu.unistra.fr>
2021-01-15 18:20:20 +0100

    added the template view, nav bar, and home page

---
+++ resources/views/shared/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="{{route("home")}}">Amical core</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{route('home')}}">Accueil<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gestion des produits</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gestion des evenements</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gestion interne</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
