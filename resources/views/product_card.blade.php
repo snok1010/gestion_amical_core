4cde390611e756f7ea3202fda22f63f7edd78783
v.bloch <valentin.bloch@etu.unistra.fr>
2021-01-18 13:52:23 +0100

    modified the view for display the products card

--- resources/views/product_card.blade.php
+++ resources/views/product_card.blade.php
@@ -1,9 +1,11 @@
 <div class="card" style="width: 300px; margin: 20px">
-	<img class="card-img-top img-fluid" src="{{$product->product_path_img}}" alt="Card image cap">
-	<div class="card-block">
-		<h4 class="card-title"  style="margin-left: 5px">{{$product->product_name}}</h4>
-		<p class="card-text" style="margin-left: 5px">Prix: {{$product->product_price}}</p>
-		<p class="card-text" style="margin-left: 5px">Stocks: {{$product->product_stocks}}</p>
-		<p class="card-text" style="margin-left: 5px">Date de peremption: {{$product->product_expiry_date}}</p>
-	</div>
+	<button onclick="totalCalculation('{{$product->product_name}}', {{$product->product_price}})">
+		<img class="card-img-top img-fluid" src="{{ asset($product->product_path_img) }}" alt="Card image cap">
+		<div class="card-block">
+			<h4 class="card-title"  style="margin-left: 5px">{{$product->product_name}}</h4>
+			<p class="card-text" style="margin-left: 5px">Prix: {{$product->product_price}}</p>
+			<p class="card-text" style="margin-left: 5px">Stocks: {{$product->product_stocks}}</p>
+			<p class="card-text" style="margin-left: 5px">Date de peremption: {{$product->product_expiry_date}}</p>
+		</div>
+	</button>
 </div>
